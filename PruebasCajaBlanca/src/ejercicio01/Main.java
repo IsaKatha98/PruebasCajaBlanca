343fd076489d8ab7c17df176c4c5b738acde6b66
IsaKatha98 <isabel.loerzer@iesnervion.es>
2023-03-14 21:42:36 +0100

    Me faltan los tests

---
+++ PruebasCajaBlanca/src/ejercicio01/Main.java
@@ -0,0 +1,19 @@
+package ejercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int num=7;
+		int resultado;
+		
+		SumaNumeros suma= new SumaNumeros();
+		
+		resultado= suma.sumaNumeros(num);
+		
+		System.out.println(resultado);
+		
+		
+	}
+
+}
---
+++ PruebasCajaBlanca/src/ejercicio01/TestSumaNumeros.java
@@ -0,0 +1,14 @@
+package ejercicio01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestSumaNumeros {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
